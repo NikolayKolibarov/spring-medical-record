23f84a31f9589c39b3c677f313df8af99b82d32d
nikolaykolibarov <nikolaykolibarov@gmail.com>
2020-01-26 01:10:06 +0200

    Added some styling.

--- src/main/java/nnk/areas/users/repositories/UserRepository.java
+++ src/main/java/nnk/areas/users/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import nnk.areas.users.entities.User;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
